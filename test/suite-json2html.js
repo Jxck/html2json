37ac3504778c9e5df25c50ef8df9a214554c71aa
Jxck <block.rxckin.beats@gmail.com>
2011-10-02 05:44:38 +0900

    add empty tag support

--- test/suite-json2html.js
+++ test/suite-json2html.js
@@ -76,5 +76,79 @@ this.suite_json2html = {
     var expected = html;
     test.strictEqual(actual, expected);
     test.done();
+  },
+  'should parse div with nested child': function(test) {
+    var json = {
+      tag: 'div',
+      child: [{
+        tag: 'p',
+        child: [{
+          tag: 'textarea'
+        }]
+      }]
+    };
+    var html = '<div><p><textarea></textarea></p></div>';
+
+    var actual = json2html(json);
+    var expected = html;
+    test.strictEqual(actual, expected);
+    test.done();
+  },
+  'should parse div with 2 nested child': function(test) {
+    var json = {
+      tag: 'div',
+      child: [{
+        tag: 'p',
+        child: [{
+          tag: 'textarea'
+        }]
+      },{
+        tag: 'p'
+      }]
+    };
+    var html = '<div><p><textarea></textarea></p><p></p></div>';
+
+    var actual = json2html(json);
+    var expected = html;
+    test.strictEqual(actual, expected);
+    test.done();
+  },
+  'should parse div with unary & ingored inline tag': function(test) {
+    var json = {
+      tag: 'div',
+      attr: {
+        id: '1',
+        class: ['foo', 'bar']
+      },
+      child: [{
+        tag: 'h2',
+        text: 'sample text'
+      },{
+        tag: 'input',
+        attr: {
+          id: 'execute',
+          type: 'button',
+          value: 'execute'
+        }
+      },{
+        tag: 'img',
+        attr: {
+          src: 'photo.jpg',
+          alt: 'photo'
+        }
+      }]
+    };
+
+    var html = ''
+      + '<div id="1" class="foo bar">'
+      + '<h2>sample text</h2>'
+      + '<input id="execute" type="button" value="execute"/>'
+      + '<img src="photo.jpg" alt="photo"/>'
+      + '</div>';
+
+    var actual = json2html(json);
+    var expected = html;
+    test.strictEqual(actual, expected);
+    test.done();
   }
 };
