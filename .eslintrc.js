705310eb13dadd4b73723208935a4d40b616ba08
Jxck <block.rxckin.beats@gmail.com>
2016-04-03 11:29:40 +0900

    ignore no-console in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,33 @@
 module.exports = {
-    "env": {
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-undef": [
-            "off"
-        ],
-        "comma-dangle": [
-            "error",
-            "only-multiline"
-        ],
-    }
+  "env": {
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-undef": [
+      "off"
+    ],
+    "comma-dangle": [
+      "error",
+      "only-multiline"
+    ],
+    "no-console": [
+      "off"
+    ]
+  }
 };
